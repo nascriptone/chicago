38e26f046ee8033da93753a27f2e325a6dc8ea56
nascriptone <scriptgoat609@gmail.com>
2024-08-07 17:15:51 +0530

    --ament

--- app/src/main/java/com/example/chicago/ChicagoScreen.kt
+++ app/src/main/java/com/example/chicago/ChicagoScreen.kt
@@ -2,22 +2,18 @@ package com.example.chicago
 
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.rememberScrollState
-import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
 import com.example.chicago.ui.screens.DetailsScreen
 import com.example.chicago.ui.screens.HomeScreen
 import com.example.chicago.ui.screens.RecomScreen
-import com.example.chicago.ui.theme.ChicagoTheme
 
 
 enum class ChicagoScreen {
@@ -50,7 +46,6 @@ fun ChicagoApp() {
             startDestination = ChicagoScreen.Home.name,
             modifier = Modifier
                 .fillMaxSize()
-                .verticalScroll(rememberScrollState())
                 .padding(innerPadding),
         ) {
             composable(route = ChicagoScreen.Home.name) {
@@ -85,7 +80,8 @@ fun ChicagoApp() {
 //        ) {
 //            NavHost(
 //                navController = rememberNavController(),
-//                modifier = Modifier.padding(it),
+//                modifier = Modifier
+//                    .padding(it),
 //                startDestination = ChicagoScreen.Home.name
 //            ) {
 //                composable(route = ChicagoScreen.Home.name) {
